76099c8b4294083f64e9cb040370a0cea9ca1d8c
Ben Holmes <bholmesdev@gmail.com>
2025-09-01 18:57:48 -0400

    refactor back to useStore API

--- src/react.ts
+++ src/react.ts
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import type { StateObject, StatePrimitive, Store } from "./index.js";
+import type { Setter, StateObject, StatePrimitive, Store } from "./index.js";
 
-export function useStoreValue<T extends StateObject | StatePrimitive>(store: Store<T>): T {
+export function useStore<T extends StateObject | StatePrimitive>(store: Store<T>): [T, (setter: Setter<T>) => void] {
     const [state, setState] = useState(store.get());
     useEffect(() => {
         return store.listen(setState);
     }, [store]);
-    return state;
+    return [state, store.set];
 }
