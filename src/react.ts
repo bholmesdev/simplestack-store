3b8659eaef68372aeabea18e0ea087fc23f88bc8
Ben Holmes <bholmesdev@gmail.com>
2025-09-01 18:19:16 -0400

    change useStore to useStoreValue

--- src/react.ts
+++ src/react.ts
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import type { Setter, StateObject, StatePrimitive, Store } from "./index.js";
+import type { StateObject, StatePrimitive, Store } from "./index.js";
 
-export function useStore<T extends StateObject | StatePrimitive>(store: Store<T>): [T, (setter: Setter<T>) => void] {
+export function useStoreValue<T extends StateObject | StatePrimitive>(store: Store<T>): T {
     const [state, setState] = useState(store.get());
     useEffect(() => {
         return store.listen(setState);
     }, [store]);
-    return [state, store.set];
+    return state;
 }
